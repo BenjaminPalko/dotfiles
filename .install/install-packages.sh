8aca0c51e57c21adb2a9d0627f85340fee566a35
Benjamin Palko <benjaminpalko@hotmail.com>
2024-03-02 20:02:08 -0500

    package installation init

---
+++ .install/install-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing pacman packages..."
+_installPackagesPacman = "${packagesPacman[@]}"
+
+echo "Installing yay packages..."
+_installPackagesYay = "${packagesYay[@]}"
+
+echo "Installing brew packages..."
+_installPackagesBrew = "${packagesBrew[@]}"
+
---
+++ .install/packages/general.sh
@@ -0,0 +1,27 @@
+packagesPacman=(
+  "zsh"
+  "bitwarden"
+  "firefox"
+  "git"
+  "github-cli"
+  "lazygit"
+  "neofetch"
+  "neovim"
+  "ttf-jetbrains-mono-nerd"
+  "wget"
+  "curl"
+  "xclip"
+  "ripgrep"
+  "waybar"
+  "swaylock"
+  "swayidle"
+)
+
+packagesYay=(
+)
+
+packagesBrew=(
+  "zsh-autosuggestions"
+  "zsh-syntax-highlighting"
+  "asdf"
+)
