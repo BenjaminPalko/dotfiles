bab589f9fa7b56c7310a4891563ed524747f9439
Benjamin Palko <benjaminpalko@hotmail.com>
2024-03-02 20:02:16 -0500

    sym links init

---
+++ .install/symlink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# files
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tool-versions ~/.tool-versions
+
+# directories
+ln -s ~/dotfiles/gtk-3.0/ ~/.config/
+ln -s ~/dotfiles/hypr/ ~/.config/
+ln -s ~/dotfiles/kitty/ ~/.config/
+ln -s ~/dotfiles/neofetch/ ~/.config/
+ln -s ~/dotfiles/nvim/ ~/.config/
+ln -s ~/dotfiles/pipewire/ ~/.config/
+ln -s ~/dotfiles/swayidle/ ~/.config/
+ln -s ~/dotfiles/swaylock/ ~/.config/
+ln -s ~/dotfiles/wofi/ ~/.config/
+
