3c21b077baafc10c3a72da45e68b74bd3b3ebdcc
Benjamin Palko <benjaminpalko@hotmail.com>
2024-03-22 17:32:45 -0500

    more styling

--- hypr/scripts/start-waybar.sh
+++ hypr/scripts/start-waybar.sh
@@ -5,7 +5,7 @@ CONFIG_FILES="$HOME/.config/waybar/ $HOME/.cache/wal/"
 trap "killall waybar" EXIT
 
 while true; do
-	sleep 0.4
+	sleep 1.6
 	waybar &
 	inotifywait -e create,modify -r $CONFIG_FILES
 	killall waybar
