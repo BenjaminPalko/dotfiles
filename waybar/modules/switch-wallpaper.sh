58e37c99852c74afa36259aeb9c47f1d3bc5b9df
Benjamin Palko <benjaminpalko@hotmail.com>
2024-04-03 19:22:48 -0500

    comment

--- waybar/modules/switch-wallpaper.sh
+++ waybar/modules/switch-wallpaper.sh
@@ -6,5 +6,6 @@ wal -i ~/Wallpapers/ -n
 # that this works even when a directory is passed.
 swww img "$(< "${HOME}/.cache/wal/wal")" --transition-type wipe --transition-angle 45 --transition-fps 90
 
+# Copy cava and update config
 cp ~/.cache/wal/cava-config ~/dotfiles/cava/config && pkill -USR2 cava
 
