392147f5c81aeac1d7eec29e6ef33ec3d15e6ae0
Benjamin Palko <benjaminpalko@hotmail.com>
2024-04-14 23:48:48 +0000

    add brew to bashrc

--- .install/brew-install.sh
+++ .install/brew-install.sh
@@ -2,6 +2,8 @@
 if [[ $(command -v brew) == "" ]]; then
   echo "Installing Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /home/linuxbrew/.linuxbrew/bin/brew shellenv >> ~/.bashrc
+  source ~/.bashrc
 else
   echo "Updating Homebrew"
   brew update
