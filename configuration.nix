b6653a4252a7287d4d170d725efaa31a253a2411
Benjamin Palko <benjaminpalko@hotmail.com>
2024-07-11 09:41:16 -0400

    missing packages for nc

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,8 @@
     unzip
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    inotify-tools
+    killall
     # Hyprland
     hypridle
     hyprlock
