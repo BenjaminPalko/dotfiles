c8b480ccfd5c10cd29a76b57705ef5bf03ec7a89
Benjamin Palko <benjaminpalko@hotmail.com>
2024-07-14 23:30:04 -0400

    update symlinks

--- .install/symlink.sh
+++ .install/symlink.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-# files
-files=(
-	".bashrc"
-	".zshrc"
-	".tool-versions"
-)
-
-_symlinkFiles() {
-	for file; do
-		if [ -h ~/${file} ]; then
-			continue
-		fi
-		if [ -f ~/${file} ]; then
-			rm ~/${file}
-		fi
-		ln -s ~/dotfiles/${file} ~/${file}
-	done
-}
-
 # directories
 directories=(
 	"cava"
 	"gtk-3.0"
+	"gtk-4.0"
 	"hypr"
-	"kitty"
 	"nvim"
 	"rofi"
 	"swappy"
@@ -35,16 +16,15 @@ directories=(
 
 _symlinkDirs() {
 	for dir; do
-		if [ -h ~/.config/${dir} ]; then
+		if [ -h ~/.config/"${dir}" ]; then
 			continue
 		fi
-		if [ -d ~/.config/${dir}/ ]; then
+		if [ -d ~/.config/"${dir}"/ ]; then
 			echo "Deleting existing directory ${dir}"
-			rm -rf ~/.config/${dir}/
+			rm -rf ~/.config/"${dir}"/
 		fi
-		ln -s ~/dotfiles/${dir}/ ~/.config/
+		ln -s ~/dotfiles/"${dir}"/ ~/.config/
 	done
 }
 
-_symlinkFiles "${files[@]}"
 _symlinkDirs "${directories[@]}"
