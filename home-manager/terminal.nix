0f73c59ec779ea3deadc6692d31b77cec33aed69
Benjamin Palko <Benjaminpalko@hotmail.com>
2024-08-29 09:05:04 -0400

    move pywal to hyprland

--- home-manager/terminal.nix
+++ home-manager/terminal.nix
@@ -65,17 +65,5 @@
   home.packages = with pkgs; [
     neofetch
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-    (pkgs.writeShellScriptBin "pywal" ''
-      # -n tells `wal` to skip setting the wallpaper.
-      wal -i ~/Wallpapers/ -n
-
-      # Using feh to tile the wallpaper now.
-      # We grab the wallpaper location from wal's cache so
-      # that this works even when a directory is passed.
-      swww img "$(<"${config.home.homeDirectory}/.cache/wal/wal")" --transition-type wipe --transition-angle 45 --transition-fps 90
-
-      # Copy cava and update config
-      cp ~/.cache/wal/cava-config ~/.config/cava/config && pkill -USR2 cava
-    '')
   ];
 }
