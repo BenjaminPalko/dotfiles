dde95a31ef6e7904a2182907016f9f067735ab7d
Benjamin Palko <benjaminpalko@hotmail.com>
2024-09-30 23:13:34 -0400

    symlinks

--- .install/symlink.sh
+++ .install/symlink.sh
@@ -2,53 +2,50 @@
 
 # files
 files=(
-  ".bashrc"
-  ".zshrc"
-  ".tool-versions"
+	".bashrc"
+	",gitconfig"
+	".zshrc"
+	".tool-versions"
 )
 
 _symlinkFiles() {
-  for file; do
-    if [ -h ~/${file} ]; then
-      continue
-    fi
-    if [ -f ~/${file} ]; then
-      rm ~/${file}
-    fi
-    ln -s ~/dotfiles/${file} ~/${file}
-  done;
+	for file; do
+		if [ -h ~/${file} ]; then
+			continue
+		fi
+		if [ -f ~/${file} ]; then
+			rm ~/${file}
+		fi
+		ln -s ~/dotfiles/${file} ~/${file}
+	done
 }
 
 # directories
 directories=(
-  "cava"
-  "discocss"
-  "gtk-3.0"
-  "hypr"
-  "kitty"
-  "neofetch"
-  "nvim"
-  "pipewire"
-  "rofi"
-  "swappy"
-  "swaync"
-  "wal"
-  "waybar"
+	"cava"
+	"hypr"
+	"kitty"
+	"neofetch"
+	"nvim"
+	"rofi"
+	"swappy"
+	"swaync"
+	"wal"
+	"waybar"
 )
 
 _symlinkDirs() {
-  for dir; do
-    if [ -h ~/.config/${dir} ]; then
-      continue
-    fi
-    if [ -d ~/.config/${dir}/ ]; then
-      echo "Deleting existing directory ${dir}"
-      rm -rf ~/.config/${dir}/
-    fi
-    ln -s ~/dotfiles/${dir}/ ~/.config/
-  done;
+	for dir; do
+		if [ -h ~/.config/${dir} ]; then
+			continue
+		fi
+		if [ -d ~/.config/${dir}/ ]; then
+			echo "Deleting existing directory ${dir}"
+			rm -rf ~/.config/${dir}/
+		fi
+		ln -s ~/dotfiles/${dir}/ ~/.config/
+	done
 }
 
 _symlinkFiles "${files[@]}"
 _symlinkDirs "${directories[@]}"
-
