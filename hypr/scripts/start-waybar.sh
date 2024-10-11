4abffe41457b28f71b968a9aa0926eaf4d639d11
Benjamin Palko <benjaminpalko@hotmail.com>
2024-10-10 23:59:53 -0400

    fix waybar

--- hypr/scripts/start-waybar.sh
+++ hypr/scripts/start-waybar.sh
@@ -2,11 +2,11 @@
 
 CONFIG_FILES="$HOME/.config/waybar/ $HOME/.cache/wal/"
 
-trap "killall .waybar-wrapped" EXIT
+trap "killall waybar" EXIT
 
 while true; do
 	sleep 1.6
 	waybar &
 	inotifywait -e create,modify -r $CONFIG_FILES
-	killall .waybar-wrapped
+	killall waybar
 done
