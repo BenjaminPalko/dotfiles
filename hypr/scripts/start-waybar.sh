31f3b9e1b2192843ba2a81ea5a2c48cf2bd0e764
Benjamin Palko <benjaminpalko@hotmail.com>
2024-10-10 22:14:00 -0400

    update hyprland from nixos branch

--- hypr/scripts/start-waybar.sh
+++ hypr/scripts/start-waybar.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 CONFIG_FILES="$HOME/.config/waybar/ $HOME/.cache/wal/"
 
-trap "killall waybar" EXIT
+trap "killall .waybar-wrapped" EXIT
 
 while true; do
 	sleep 1.6
 	waybar &
 	inotifywait -e create,modify -r $CONFIG_FILES
-	killall waybar
+	killall .waybar-wrapped
 done
